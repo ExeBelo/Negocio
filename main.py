2729a4468ce6a504ba615e06b8af994e8bd8b2c2
juan pablo <insaurraldejuanpablo12@gmail.com>
2025-10-22 19:27:58 -0300

    Cambios por Manu

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ class Producto:
         self.nombre = nombre
         self.precio = precio
         self.stock = 0
+        self.descuento = 0
+
+    def agregar_descuento(self, porcentaje):
+        self.precio = self.precio * (1 - porcentaje / 100)
+
+
