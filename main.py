7a24ad84b60f1bfcf7cf8e6b15306e2881724591
isaac <gabrielzapata147@outlook.com>
2025-10-24 20:17:31 -0300

    commitIsaac

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 class Producto:
-
-    def __init__(self, nombre, precio):
+    def __init__(self, nombre, precio, stock_actual):
         self.nombre = nombre
         self.precio = precio
-        self.stock = 0
+        self.stock = stock_actual  
+
+    def comprobar_stock(self, cantidad):
+        if cantidad <= self.stock:
+            return True
+        else:
+            return False
+
+
